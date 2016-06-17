ffe372eb259417463f839e6868a81fd257f8b749
Chris Stapler <cstapler@iastat.edu>
2016-06-16 23:40:40 -0500

    Adds model and serializer for song

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    artist = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('added',)
---
+++ rest_api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_api.models import Song
+
+class SongSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    artist = serializers.CharField(required=False, allow_blank=True, max_length=100)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Song.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.artist = validated_data.get('title', instance.title)
+        instance.save()
+        return instance
