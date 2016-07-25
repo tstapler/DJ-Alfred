7e62c40e9e52cf6b02bb3bdd6a087367d1d2717a
Chris Stapler <cstapler@iastat.edu>
2016-07-24 21:37:16 -0500

    Add str method to each model
    View uses string and not unicode method to represent
    model objects therefore the addition of __str__ is needed

--- rest_api/models.py
+++ rest_api/models.py
@@ -5,22 +5,23 @@ class Song(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     artist = models.CharField(max_length=100, blank=True, default='')
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return '%d: %s' % (self.id, self.title)
+
     class Meta:
         ordering = ('submission_time',)
-
-    def __unicode__(self):
-        return '%d: %s' % (self.id, self.title)
 
 class Playlist(models.Model):
     songs = models.ManyToManyField(Song)
     name = models.CharField(max_length=100, blank=True, default='')
     owner = models.CharField(max_length=100, blank=True, default='')
     created_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return '%d: %s' % (self.id, self.name)
+
     class Meta:
         ordering = ('created_date',)
 
-    def __unicode__(self):
-        return '%d: %s' % (self.id, self.name)
 
 class Event(models.Model):
     playlists = models.ManyToManyField(Playlist)
@@ -29,6 +30,8 @@ class Event(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField(blank=True, default='')
     owner = models.CharField(max_length=100, blank=True, default='')
+    def __str__(self):
+        return '%d: %s' % (self.id, self.name)
 
     class Meta:
         ordering = ('date_time_start',)
